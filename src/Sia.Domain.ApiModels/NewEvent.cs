01d95fe564d02ea2b1e20fead92fd62f0425225a
Philip Dimitratos <pdimit@microsoft.com>
2017-10-16 15:56:14 -0700

    Added dynamic data field and automapper configuration for converting between data storageobjects with a backing string field (for a JSON column in the database) and DTOs with a true dynamic value.

--- src/Sia.Domain.ApiModels/NewEvent.cs
+++ src/Sia.Domain.ApiModels/NewEvent.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Sia.Shared.Data;
+using System;
 using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
 
 namespace Sia.Domain.ApiModels
 {
     public class NewEvent
+        :IDynamicDataSource
     {
         [Required]
         public long? EventTypeId { get; set; }
@@ -11,5 +14,6 @@ namespace Sia.Domain.ApiModels
         public DateTime? Occurred { get; set; }
         [Required]
         public DateTime? EventFired { get; set; }
+        public dynamic Data { get; set; } = new ExpandoObject();
     }
 }
--- src/Sia.Domain/Event.cs
+++ src/Sia.Domain/Event.cs
@@ -2,16 +2,20 @@
 using Sia.Shared.Data;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Text;
 
 namespace Sia.Domain
 {
-    public class Event : IEntity
+    public class Event : IEntity, IDynamicDataSource
     {
         public long Id { get; set; }
         public long? IncidentId { get; set; }
         public long EventTypeId { get; set; }
         public DateTime Occurred { get; set; }
         public DateTime EventFired { get; set; }
+        public dynamic Data { get; set; } = new ExpandoObject();
+
+
     }
 }
