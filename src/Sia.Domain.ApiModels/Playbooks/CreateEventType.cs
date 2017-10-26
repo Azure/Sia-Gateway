885f1724b3a0ce716ca5a863c9e13b87f00beec9
Philip Dimitratos <pdimit@microsoft.com>
2017-10-26 11:11:03 -0700

    Added Data on EventType and CreateEventType

--- src/Sia.Domain.ApiModels/Playbooks/CreateEventType.cs
+++ src/Sia.Domain.ApiModels/Playbooks/CreateEventType.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using Sia.Shared.Data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Sia.Domain.ApiModels.Playbooks
 {
-    public class CreateEventType
+    public class CreateEventType : IJsonDataObject
     {
         [Required]
         public string Name { get; set; }
+        [Required]
+        public object Data { get; set; }
     }
 }
--- src/Sia.Domain/Playbook/EventType.cs
+++ src/Sia.Domain/Playbook/EventType.cs
@@ -4,10 +4,11 @@ using System.Collections.Generic;
 
 namespace Sia.Domain.Playbook
 {
-    public class EventType : IEntity
+    public class EventType : IEntity, IJsonDataObject
     {
         public long Id { get; set; }
         public string Name { get; set; }
+        public object Data { get; set; }
         public ICollection<Action> Actions { get; set; }
             = new List<Action>();
     }
