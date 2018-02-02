26e20012e99e204c1716f577955fe68f76eeaaf2
Sasha Pierson <RedSquirrelious@users.noreply.github.com>
2018-02-02 15:55:24 -0800

    more detailed mock event

--- src/Sia.Gateway/Requests/Playbook/EventTypes/GetEventTypes.cs
+++ src/Sia.Gateway/Requests/Playbook/EventTypes/GetEventTypes.cs
@@ -5,6 +5,11 @@ using Sia.Gateway.Requests.Playbook;
 using Sia.Shared.Authentication;
 using Sia.Shared.Authentication.Http;
 using Sia.Shared.Requests;
+using Microsoft.Extensions.Configuration;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using Sia.Shared.Extensions.Mediatr;
 
 namespace Sia.Gateway.Requests
 {
@@ -13,6 +18,29 @@ namespace Sia.Gateway.Requests
         public GetEventTypesRequest(AuthenticatedUserContext userContext)
             : base(userContext)
         {
+        }
+    }
+
+    public class GetEventTypesShortCircuit : PlaybookShortCircuit<GetEventTypesRequest, IEnumerable<EventType>>
+    {
+        public GetEventTypesShortCircuit(IConfigurationRoot config) : base(config)
+        {
+
+        }
+
+        public override Task<IEnumerable<EventType>> GenerateMockAsync(GetEventTypesRequest request, CancellationToken cancellationToken)
+        {
+            var eventType = new EventType()
+            {
+                Id = 100000000,
+                Name = "This is a mock",
+                Data = new MockEventTypeData()
+            };
+            var result = new List<EventType>
+            {
+                eventType
+            };
+            return Task.FromResult(result.AsEnumerable());
         }
     }
 
