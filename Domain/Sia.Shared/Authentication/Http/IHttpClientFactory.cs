74fed900fcc9fb12b7c5b04968c644aa4ae03f22
Alberto Rodríguez <alberto.c.rodriguez@gmail.com>
2018-02-12 10:20:50 -0800

    Squashed 'domain/' content from commit faa38d8
    git-subtree-dir: domain
    git-subtree-split: faa38d809c722b27e37c25b94e2e1891c56ba25f

---
+++ Domain/Sia.Shared/Authentication/Http/IHttpClientFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Sia.Shared.Authentication
+{
+    public interface IHttpClientFactory
+    {
+        Task<HttpClient> GetClientAsync();
+    }
+}
