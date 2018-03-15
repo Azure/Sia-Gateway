90ef54ebdd1875015c9156006c201a0341dee6ff
Philip Dimitratos <pdimit@microsoft.com>
2018-03-15 10:25:57 -0700

    Added more test cases for filters

--- test/Sia.Gateway.Tests/Filters/EventFiltersTests.cs
+++ test/Sia.Gateway.Tests/Filters/EventFiltersTests.cs
@@ -292,5 +292,72 @@ namespace Sia.Gateway.Tests.Filters
             Assert.AreEqual(0, result.Count());
         }
 
+        [TestMethod]
+        public void Filter_WhenProvidedOnlyIncidentId_IsMatchFor_EventWithSameIncidentId()
+        {
+            var serviceUnderTest = new EventFilters()
+            {
+                IncidentId = 1
+            };
+            var testInput = new Event()
+            {
+                IncidentId = 1
+            };
+
+            var result = serviceUnderTest.IsMatchFor(testInput);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Filter_WhenProvidedOnlyIncidentId_IsNotMatchFor_EventWithDifferentIncidentId()
+        {
+            var serviceUnderTest = new EventFilters()
+            {
+                IncidentId = 1
+            };
+            var testInput = new Event()
+            {
+                IncidentId = 2
+            };
+
+            var result = serviceUnderTest.IsMatchFor(testInput);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Filter_WhenProvidedOnlyEventTypes_IsMatchFor_EventWithIncludedEventType()
+        {
+            var serviceUnderTest = new EventFilters()
+            {
+                EventTypes = new long[] { 1, 3 }
+            };
+            var testInput = new Event()
+            {
+                EventTypeId = 1
+            };
+
+            var result = serviceUnderTest.IsMatchFor(testInput);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Filter_WhenProvidedOnlyEventTypes_IsNotMatchFor_EventWithNotIncludedEventType()
+        {
+            var serviceUnderTest = new EventFilters()
+            {
+                EventTypes = new long[] { 1, 3 }
+            };
+            var testInput = new Event()
+            {
+                EventTypeId = 2
+            };
+
+            var result = serviceUnderTest.IsMatchFor(testInput);
+
+            Assert.IsFalse(result);
+        }
     }
 }
